a5cca0294f9700632f951ce3e808570f0e22d77b
Gideon Aleonogwe <galosikhena@gmail.com>
2024-07-24 12:27:58 +0100

    removed the install dependencies block and a few other adjustments

--- install_devopsfetch.sh
+++ install_devopsfetch.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# devopsfetch - Installation Script
+#--------------------------
+# Function to echo in green
+#--------------------------
+green_echo() {
+     echo -e "\e[32m$1\e[0m"
+}
 
+#-------------------------------
 # Check if script is run as root
+#-------------------------------
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit 1
 fi
 
-# Install dependencies
-apt-get update
-apt-get install -y docker.io nginx
-
+#------------------------------------------
 # Copy devopsfetch script to /usr/local/bin
+#------------------------------------------
 cp devopsfetch /usr/local/bin/
 chmod +x /usr/local/bin/devopsfetch
 
+#----------------------------
 # Create systemd service file
+#----------------------------
 cat << EOF > /etc/systemd/system/devopsfetch.service
 [Unit]
 Description=DevOpsFetch Monitoring Service
@@ -31,7 +38,9 @@ User=root
 WantedBy=multi-user.target
 EOF
 
+#----------------------------------
 # Create log rotation configuration
+#----------------------------------
 cat << EOF > /etc/logrotate.d/devopsfetch
 /var/log/devopsfetch.log {
     daily
@@ -44,11 +53,13 @@ cat << EOF > /etc/logrotate.d/devopsfetch
 }
 EOF
 
+#---------------------------------------------
 # Reload systemd, enable and start the service
+#---------------------------------------------
 systemctl daemon-reload
 systemctl enable devopsfetch.service
 systemctl start devopsfetch.service
 
-echo "DevOpsFetch has been installed and service started."
-echo "Logs are stored in /var/log/devopsfetch.log"
-echo "Use 'devopsfetch -h' for usage instructions."
+green_echo "DevOpsFetch has been installed and service started."
+green_echo "Logs are stored in /var/log/devopsfetch.log"
+green_echo "Use 'devopsfetch -h' for usage instructions."
